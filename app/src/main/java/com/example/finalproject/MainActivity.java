6f8e5fdedbb084990a1548d62824258127aa50a3
maiverta <mayver3@gmail.com>
2023-03-12 08:49:16 +0200

    feat: basic app pages

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.finalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//
+//    NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.main_navhost);
+//    navController = navHostFragment.getNavController();
+//        NavigationUI.setupActionBarWithNavController(this,navController);
+//
+//    BottomNavigationView navView = findViewById(R.id.main_bottomNavigationView);
+//        NavigationUI.setupWithNavController(navView,navController);
+//
+//    int fragmentMenuId = 0;
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu,menu);
+//        if (fragmentMenuId != 0){
+//            menu.removeItem(fragmentMenuId);
+//        }
+//        fragmentMenuId = 0;
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//
+//        if (item.getItemId() == android.R.id.home){
+//            navController.popBackStack();
+//        }else{
+//            fragmentMenuId = item.getItemId();
+//            return NavigationUI.onNavDestinationSelected(item,navController);
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+}
---
+++ app/src/main/java/com/example/finalproject/ObjectListFragment.java
@@ -0,0 +1,98 @@
+package com.example.finalproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ObjectListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ObjectListFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ObjectListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ObjectListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ObjectListFragment newInstance(String param1, String param2) {
+        ObjectListFragment fragment = new ObjectListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_object_list, container, false);
+
+        ListView list = view.findViewById(R.id.object_list);
+        list.setAdapter(new ObjectListAdapter());
+
+        return view;
+    }
+
+    class ObjectListAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            if(view == null){
+                view = getLayoutInflater().inflate(R.layout.object_list_row, null);
+            }
+
+            return view;
+        }
+    }
+}
